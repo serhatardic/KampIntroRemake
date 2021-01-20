ac774b0379fec9a9adfa45dec8ed760ed84e05d8
Serhat <Serhat@DESKTOP-QGTQQ9R>
2021-01-20 23:57:40 +0300

    Add project files.

---
+++ Collections/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ctrl k + ctrl c ---- ctrl k + ctrl u
+
+            
+            List<string> isimler2 = new List<string> { "Engin", "Murat", "Kerem", "Halil" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("İlker");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+        }
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Engin");
+
+            Console.WriteLine(isimler);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
